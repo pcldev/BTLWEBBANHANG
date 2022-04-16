e8753bad39eb63318758781a2b2ee12649eb0826
Phan Cong Long <pcl2k2@gmail.com>
2022-04-16 15:54:52 +0700

    Add cmt

--- BTLWEBBANHANG/Global.asax.cs
+++ BTLWEBBANHANG/Global.asax.cs
@@ -12,7 +12,7 @@ namespace BTLWEBBANHANG
 
         protected void Application_Start(object sender, EventArgs e)
         {
-            // Update
+            // Update12
         }
 
         protected void Session_Start(object sender, EventArgs e)
