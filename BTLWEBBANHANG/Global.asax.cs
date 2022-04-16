e55bc594e7d4888327778d72d98b6887ad126c65
Phan Cong Long <pcl2k2@gmail.com>
2022-04-16 16:23:41 +0700

    Add cmtasdasd

--- BTLWEBBANHANG/Global.asax.cs
+++ BTLWEBBANHANG/Global.asax.cs
@@ -13,6 +13,7 @@ namespace BTLWEBBANHANG
         protected void Application_Start(object sender, EventArgs e)
         {
             // Update12
+            // asdasd
         }
 
         protected void Session_Start(object sender, EventArgs e)
