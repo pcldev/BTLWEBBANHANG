4d5c2ee9b4e401466809849ad13fa8753f494321
Phan Cong Long <pcl2k2@gmail.com>
2022-04-17 11:19:28 +0700

    Add img property to product class

--- BTLWEBBANHANG/Product.cs
+++ BTLWEBBANHANG/Product.cs
@@ -10,19 +10,22 @@ namespace BTLWEBBANHANG
         private int id;
         private string name;
         private string description;
+        private string image;
         private long price;
 
-        public Product(int id , string name, string description , long price)
+        public Product(int id , string name, string description ,string image ,long price )
         {
             this.id = id;
             this.name = name;
             this.description = description;
+            this.Image = image;
             this.price = price;
         }
 
         public int Id { get => id; set => id = value; }
         public string Name { get => name; set => name = value; }
         public string Description { get => description; set => description = value; }
+        public string Image { get => image; set => image = value; }
         public long Price { get => price; set => price = value; }
     }
 }
